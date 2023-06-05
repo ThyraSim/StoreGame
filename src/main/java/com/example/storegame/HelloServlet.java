64d642b9b14413137f30ec5fe74a3d0a172326cd
ThyraSim <simbeaudry@live.com>
2023-06-05 10:42:06 -0400

    Ajout MessagesBundle

--- src/main/java/com/example/storegame/HelloServlet.java
+++ src/main/java/com/example/storegame/HelloServlet.java
@@ -24,7 +24,6 @@ public class HelloServlet extends HttpServlet {
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
     }
-
     public void destroy() {
     }
 }
