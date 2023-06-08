a24b8e7daf79104029745d924799339a9b1813a7
Jean-Dan10 <jean.daniel.laplante@gmail.com>
2023-06-08 15:45:49 -0400

    Mapping, load on start et début du script

---
+++ src/main/java/config/ConfigStartServlet.java
@@ -0,0 +1,26 @@
+package config;
+
+
+import javax.servlet.ServletException;
+
+import javax.servlet.http.HttpServlet;
+
+
+
+    public class ConfigStartServlet extends HttpServlet {
+
+        public void init() throws ServletException {
+
+
+                System.out.println("Lancement ConfigStartServlet ");
+
+            }
+        }
+
+
+
+
+
+
+
+
